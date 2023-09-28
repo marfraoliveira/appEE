62cf15c008e851a8a8feb26b545f5b47d8de1be4
marfraoliveira <128929566+marfraoliveira@users.noreply.github.com>
2023-09-28 14:08:13 -0300

    novodeploy

--- app.py
+++ app.py
@@ -30,12 +30,12 @@ def predict():
         # Obtém o JSON da solicitação
         data = request.get_json()
         # Pré-processamento dos dados
-        #columns = ['x', 'y', 'z']
-        #df = pd.DataFrame(data, columns=columns)
-        #df['x'] = df['x'].astype('float')
-        #df['y'] = df['y'].astype('float')
-        #df['z'] = df['z'].astype('float')
-        #data = df.to_numpy()
+        columns = ['x', 'y', 'z']
+        df = pd.DataFrame(data, columns=columns)
+        df['x'] = df['x'].astype('float')
+        df['y'] = df['y'].astype('float')
+        df['z'] = df['z'].astype('float')
+        data = df.to_numpy()
         #data = data.reshape(-1, 90, 3)
         # Faça uma única previsão com o modelo carregado
         #class_predict = np.argmax(model.predict(data), axis=1)
